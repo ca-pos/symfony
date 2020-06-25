4c53c71d7bc3869fa7aff92c365873745f3cb7cc
capos <camille-aime.possamai@wanadoo.fr>
2020-06-25 22:38:52 +0200

    Plusieurs refactorisations du code, ajout des pages autres-pages-simples et base-de-donnees.etc

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 
 require_once 'vendor/autoload.php';
 
+use Cocur\Slugify\Slugify;
+
 // Routage
 
-$page = 'home';
+$page = 'accueil';
+
 if(isset($_GET['p'])) {
     $page = $_GET['p'];
 }
@@ -17,16 +20,34 @@ $twig = new \Twig\Environment($loader, [
     'cache' => false, // __DIR__ . '/templates'kk
 ]);
 
+$ext = '.html.twig';
+$pages_h1 = [
+    "Accueil",
+    "Mise en place du projet",
+    "Première page",
+    "Autres pages simples",
+    "Base de données et création de fausses données",
+];
 
-switch($page) {
-    case 'home':
-        echo $twig->render('home.html.twig', ['page' => 'home']);
-        break;
-    case 'mise_en_place':
-        echo $twig->render('mise-en-place-du-projet.html.twig');
-        break;
-    default:
-        $header('HTTP/1.0 404 Not found');
-        echo $twig->render('404.twig');
-        break;             
+$slug = new Slugify();
+$pages_slug_list = [];
+
+foreach ( $pages_h1 as $page_tmp ) {
+    $pages_slug_list[] = $slug->slugify( $page_tmp );
 }
+
+$page_num = array_search($page, $pages_slug_list);
+
+$twig->addGlobal('pages_list', $pages_slug_list);
+$twig->addGlobal('pages_h1', $pages_h1);
+$twig->addGlobal('page_number', $page_num);
+
+echo $twig->render($pages_slug_list[$page_num].$ext, [
+                'page' => $page,
+            ]);
+
+
+//$slug = new Slugify();
+//echo $slug->slugify("Mise en place du projet");
+
+//$twig->addGlobal('current_page', $page);
