41e3316bbe36ae06b27efa8ac1e2bdf9106aeb8b
capos <camille-aime.possamai@wanadoo.fr>
2020-07-12 14:49:30 +0200

    ajout de l'organisation en section de la page d'accueil

--- AppTwigExtension.php
+++ AppTwigExtension.php
@@ -7,37 +7,82 @@ class AppTwigExtension extends AbstractExtension {
     public function getFunctions()
     {
         return [
-            new \Twig\TwigFunction('findPageNumber', [
-                $this, 'pageNumber'
-                ]),
+            // new \Twig\TwigFunction('findPage', [
+            //     $this, 'findPage'
+            //     ]),
             new \Twig\TwigFunction('nextPage', [
                 $this, 'nextPage'
                 ]),
             new \Twig\TwigFunction('previousPage', [
                $this, 'previousPage'
                 ]),
-    
+            // new \Twig\TwigFunction('findPageNumber', [
+            //     $this, 'findPageNumber'
+            //     ]),
         ];
     }
 
-    public function pageNumber( $slug, $pages_slug) {
-        $index = array_search($slug, $pages_slug);
-         return  $index;
+    /**
+     * renvoie le slug de la page suivante
+     *
+     * @param string $section
+     * @param int $current
+     * @param array $pages_slug
+     * @return string
+     */
+    public function nextPage( $section, $current,  $pages_slug) {
+
+        $maxIndexPage = count($pages_slug[$section]);
+        $maxIndexSection = count( array_keys($pages_slug));
+        $current++;
+        // la page suivante est dans la même section
+        if( $current < $maxIndexPage ) {
+
+            return $pages_slug[$section][$current];
+        }
+        // on est à la dernière page d'une section
+        // si on n'est pas à la dernière section,
+        // la page suivante est la première page de la section suivante
+        elseif (array_search($section, array_keys($pages_slug)) < $maxIndexSection -  1) {
+            $index = array_search($section, array_keys($pages_slug)) + 1;
+            $section = array_keys($pages_slug)[$index];
+
+            return $pages_slug[$section][0];
+        }
+        // si on est à la dernière page de la dernière section
+        // on revient à l'accueil
+        else {
+            return 'accueil';
+        }
     }
+    /**
+     * renvoie le slug de la page précédente
+     *
+     * @param string $section
+     * @param int $current
+     * @param array $pages_slug
+     * @return string
+     */
+    public function previousPage( $section, $current, $pages_slug) {
+        // la page précédente est dans la même section
+        if( $current > 0) {
+            return $pages_slug[$section][--$current];
+        }
+        // on est à la première page d'une section
+        // si ce n'est pas la première section
+        // la page précédente est la dernière page de la section précédente
+        elseif ( array_search($section, array_keys($pages_slug)) > 0 ) {
+            $index = array_search($section, array_keys($pages_slug)) - 1;
+            $section = array_keys($pages_slug)[$index];
+            $lastPageNumber = count($pages_slug[$section]) - 1;
 
-    public function nextPage( $slug, $pages_slug) {
-        $maxIndex = count($pages_slug)-1;
-        $page = $this->pageNumber( $slug, $pages_slug);
-        $result = ($page >= $maxIndex)?0:+$page+1;
-        
-        return $result;
-    }
-
-    public function previousPage( $slug, $pages_slug) {
-        $page = $this->pageNumber( $slug, $pages_slug);
-        $result = ($page <= 0)?0:+$page-1;
-
-        return $result;
+            return $pages_slug[$section][$lastPageNumber];
+        }
+        // si on est à la première page de la première section
+        // on revient à l'accueil
+        else {
+            return 'accueil';
+        }
     }
 
 
--- index.php
+++ index.php
@@ -4,69 +4,104 @@ require_once 'vendor/autoload.php';
 require 'AppTwigExtension.php';
 
 use Cocur\Slugify\Slugify;
-
-// Routage
-
-$page = 'accueil';
-
-if(isset($_GET['p'])) {
-    $page = $_GET['p'];
-}
-
-
-// Rendu de templates
+ 
 
 $loader = new \Twig\Loader\FilesystemLoader( __DIR__ . '/templates');
 $twig = new \Twig\Environment($loader, [
     'cache' => false, // __DIR__ . '/templates'kk
 ]);
-
 $twig->addExtension(new AppTwigExtension());
 
-$ext = '.html.twig';
+// Routage
+
+// la première fois ($_GET unset), on se rend à l'accueil
+// ensuite, ($_GET set) pointe sur la page où aller (lien de la page accueil)
+if(isset($_GET['p'])) {
+    $page = $_GET['p'];
+} 
+else {
+    $page='accueil';
+}
+// tableau associatif (key => value) contenant les pages disponibles, 
+// organisées en 2 sections : "Pas à pas" et "Exemples"
+//
+// Les titres de section sont les keys
+// Les values sont les intitulés sous forme de tableau des 
+// pages de la section
+//
+// D'autres sections pourront être ajoutées de la même manière
 $pages_h1 = [
-    "Accueil",
-    "Mise en place du projet",
-    "Première page",
-    "Autres pages simples",
-    "Base de données et création de fausses données",
-    "Suppression d'un attribut dans une table",
-    "Formulaires",
-    "Insérer getConfiguration dans une classe",
-    "Validation de formulaire",
-    "Unicité d'un élément",
-    "Création de la table User",
-    "Relation entre tables",
-    "Créer les faux utilisateurs",
-    "Encryptage des mots de passe",
-    "Page de connexion",
-    "Déconnexion",
-    "Page d'inscription",
-    "Barre de navigation adaptative",
-    "Page de modification de profil",
-    "Page de modification du mot de passe",
-    "Ajouter une erreur à un champ",
+    "Pas à pas" =>  [ //"Accueil",
+     "Mise en place du projet",
+     "Première page",
+     "Autres pages simples",
+     "Base de données et création de fausses données",
+     "Suppression d'un attribut dans une table",
+     "Formulaires",
+     "Insérer getConfiguration dans une classe",
+     "Validation de formulaire",
+     "Unicité d'un élément",
+     "Création de la table User",
+     "Relation entre tables",
+     "Créer les faux utilisateurs",
+     "Encryptage des mots de passe",
+     "Page de connexion",
+     "Déconnexion",
+     "Page d'inscription",
+     "Barre de navigation adaptative",
+     "Page de modification de profil",
+     "Page de modification du mot de passe",
+     "Ajouter une erreur à un champ",
+        ],
+     "Exemples" => [
+         "getConfiguration",
+        ],
+    "Ligne de commande" => [
+        "composer",
+        "php bin/console",
+    ],
 ];
 
-$slug = new Slugify();
-$pages_slug_list = [];
 
-foreach ( $pages_h1 as $page_tmp ) {
-    $pages_slug_list[] = $slug->slugify( $page_tmp );
+// Rendu de templates
+
+// parcourt les titres de chapitre (for) des différentes sections (foreach)
+// en détermine le slug et range ces slugs dans un tableau page_slug_list
+// de même structure (mêmes keys) que le tableau d'origine
+// une fois qu'une section est "slugifiée", le array_search cherche si la 
+// page à atteindre est dans cette section
+//
+// QUAND LA LISTE SERA DÉFINITIVE, IL NE FAUDRA PLUS LE FAIRE À CHAQUE FOIS
+// SI C'EST POSSIBLE !!!
+$slug = new Slugify();
+foreach ( $pages_h1 as $col => $page_tmp ) {
+    for( $i = 0, $nb=count($page_tmp); $i < $nb; $i++) {
+        $pages_slug_list[$col][$i] = $slug->slugify($page_tmp[$i]);
+    }
 }
 
-$page_num = array_search($page, $pages_slug_list);
+foreach( $pages_h1 as $col => $page_tmp ) {
+    $page_num[$col] = array_search($page, $pages_slug_list[$col]);
+}
+$keys = array_keys($page_num);
+$nb = count($keys);
+$val = -1;      // pour éviter un warning sur la page d'accueil où le if ci-dessous est sauté
+for( $i = 0; $i < $nb; $i++) {
+    if ( ($page_num[$keys[$i]]) !== false ) {
+        $col = $keys[$i];
+        $val =  $page_num[$keys[$i]];
+    }
+}
 
-$twig->addGlobal('pages_list', $pages_slug_list);
 $twig->addGlobal('pages_h1', $pages_h1);
-$twig->addGlobal('page_number', $page_num);
+$twig->addGlobal('pages_slug', $pages_slug_list);
 
-echo $twig->render($pages_slug_list[$page_num].$ext, [
-                'page' => $page,
-            ]);
+$ext = '.html.twig';
+$pageToGo = $page . $ext;
 
+echo $twig->render($pageToGo, [
+    'slug' => $page,
+    'section' => $col,
+    'current' => $val,
+    ]);
 
-//$slug = new Slugify();
-//echo $slug->slugify("Mise en place du projet");
-
-//$twig->addGlobal('current_page', $page);
